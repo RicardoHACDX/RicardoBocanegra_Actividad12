2e982a17e6530c19b76843498f5c1eb50ae2684f
RicardoBocanegra <ricardomendez145@gmail.com>
2020-11-18 12:50:14 -0600

    Funcion

---
+++ funciones.js
@@ -0,0 +1,9 @@
+const number = 115;
+const string = "Lideres de Mercado";
+const boolean = false;
+let sinvalor;
+console.log (typeof number);
+console.log (typeof string);
+console.log (typeof boolean);
+console.log (typeof sinvalor);
+
